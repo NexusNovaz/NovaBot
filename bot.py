fa2fc5a256b3c301a7962e3b1c48a098d009d2f8
Nexus Novaz <19374500+NexusNovaz@users.noreply.github.com>
2018-08-31 14:20:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,9 +31,8 @@ async def on_ready():
 
 # ON MEMBER JOIN
 @bot.listen()
-async def on_member_join(ctx, member):
+async def on_member_join(member):
     await member.edit(nick="LVL 1 | "+ member.name)
-    await member.add_roles(discord.utils.get(ctx.guild.roles, name="LVL 1"))
 # END ON MEMBER JOIN
 
 # COGS
